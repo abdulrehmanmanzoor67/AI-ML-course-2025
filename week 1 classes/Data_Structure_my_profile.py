1936d547b89d5f32b736ced6ea323564f21f19be
abdulrehmanmanzoor67 <abdulrehmanmanzoor67@hotmail.com>
2025-07-09 20:38:22 +0500

    week 1 leacture

---
+++ week 1 classes/Data_Structure_my_profile.py
@@ -0,0 +1,50 @@
+#Data Structues in python
+
+# List in python
+my_profile = ["Abdul Rehman Manzoor", 22 , 5.9 ]
+
+print(my_profile)
+
+print(type(my_profile))
+
+
+for i in my_profile:
+    print(i)
+
+
+# Tuples in python
+
+myprofile = ("abdul rehman manzoor",22 , 5.9 )
+
+print(myprofile)
+
+print(type(myprofile))
+
+for item in myprofile:
+    print(item)
+
+print(myprofile[0])
+print(myprofile[2])
+
+
+# Set in python
+
+my_profile1 = {"Abdul Rehman Manzoor", 22, 5.9}
+
+print(my_profile1)
+
+print(type(my_profile1))
+
+for it in my_profile1:
+    print(it)
+
+
+# dictionary in python
+
+my_profile2 = {'Name': "Abdul Rehman Manzoor", 'Age': 22 , 'Height': 5.9}
+print(my_profile2["Name"])
+print(my_profile2["Age"])
+print(my_profile2["Height"])
+
+print(type(my_profile2))
+
